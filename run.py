97389065bdfe276ef1293d57935251e7ac7c82d6
TA899 <96488576+TA899@users.noreply.github.com>
2024-11-06 17:12:02 -0300

    Delete run.py

--- run.py
+++
@@ -1,7 +0,0 @@
-from trello import create_app  # Asegúrate de que este import sea correcto
-
-app = create_app()  # Crea la aplicación utilizando la función create_app
-
-if __name__ == '__main__':
-   #app.run(debug=True)  # Ejecuta la aplicación, comentado para produccion 
- pass 
